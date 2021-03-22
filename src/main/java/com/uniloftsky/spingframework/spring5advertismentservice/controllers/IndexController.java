58a274decaa97b27f222f282239c337562408db2
uniloftsky <uniloftsky@gmail.com>
2021-03-22 23:41:58 +0200

    injected front-end. closes #1

---
+++ src/main/java/com/uniloftsky/spingframework/spring5advertismentservice/controllers/IndexController.java
@@ -0,0 +1,34 @@
+package com.uniloftsky.spingframework.spring5advertismentservice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "/index"})
+    public String getIndexPage() {
+        return "index";
+    }
+
+    @GetMapping({"/catalog", "catalog"})
+    public String getCatalogPage() {
+        return "catalog";
+    }
+
+    @GetMapping({"/item", "item"})
+    public String getItemPage() {
+        return "item";
+    }
+
+    @GetMapping({"/profile", "profile"})
+    public String getProfilePage() {
+        return "profile";
+    }
+
+    @GetMapping({"/companyProfile", "companyProfile"})
+    public String getCompanyProfilePage() {
+        return "companyProfile";
+    }
+
+}
