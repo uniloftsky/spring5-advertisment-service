4125f985655330bc5addd4ce2576ae2f7a21caba
uniloftsky <uniloftsky@gmail.com>
2021-03-22 20:07:07 +0200

    project added

---
+++ src/main/java/com/uniloftsky/spingframework/spring5advertismentservice/Spring5AdvertismentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.spingframework.spring5advertismentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5AdvertismentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5AdvertismentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniloftsky/spingframework/spring5advertismentservice/Spring5AdvertismentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.spingframework.spring5advertismentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5AdvertismentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
