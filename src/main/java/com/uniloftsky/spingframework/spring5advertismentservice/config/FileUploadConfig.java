db27c511d6041b47ec86c5d03b364d04e4a6af9f
uniloftsky <uniloftsky@gmail.com>
2021-03-25 01:27:27 +0200

    preparing for image uploading

---
+++ src/main/java/com/uniloftsky/spingframework/spring5advertismentservice/config/FileUploadConfig.java
@@ -0,0 +1,21 @@
+package com.uniloftsky.spingframework.spring5advertismentservice.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
+public class FileUploadConfig {
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.parse("20MB"));
+        factory.setMaxRequestSize(DataSize.parse("20MB"));
+        return factory.createMultipartConfig();
+    }
+
+}
