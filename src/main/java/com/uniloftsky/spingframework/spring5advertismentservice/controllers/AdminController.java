d68ab21dabca229009dfba5fcf1c43a8e55ca902
uniloftsky <uniloftsky@gmail.com>
2021-03-26 21:48:27 +0200

    pagination for check ads on admin-panel

--- src/main/java/com/uniloftsky/spingframework/spring5advertismentservice/controllers/AdminController.java
+++ src/main/java/com/uniloftsky/spingframework/spring5advertismentservice/controllers/AdminController.java
@@ -1,12 +1,18 @@
 package com.uniloftsky.spingframework.spring5advertismentservice.controllers;
 
+import com.uniloftsky.spingframework.spring5advertismentservice.model.Advertisement;
 import com.uniloftsky.spingframework.spring5advertismentservice.services.AdvertisementService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class AdminController {
@@ -19,7 +25,13 @@ public class AdminController {
 
     @GetMapping("/admin")
     public String getAdminPage(@PageableDefault(size = 6, sort = "id", direction = Sort.Direction.DESC) Pageable pageable, Model model) {
-        model.addAttribute("checkAds", advertisementService.findAllCheckingPage(pageable));
+        Page<Advertisement> pages = advertisementService.findAllCheckingPage(pageable);
+        Integer maxPage = pages.getTotalPages();
+        model.addAttribute("maxPage", maxPage);
+        model.addAttribute("ads", pages);
+        List<Integer> pageNumbers = IntStream.rangeClosed(1, pages.getTotalPages()).boxed().collect(Collectors.toList());
+        model.addAttribute("pageNumbers", pageNumbers);
+        model.addAttribute("currentPage", pages.getNumber());
         return "admin/index";
 
     }
